3e00a661d0f93c0a652de5de28a650a50504eb42
Ovy95 <jackoverton21@hotmail.com>
2020-03-18 21:31:22 +0000

    Spec refactor use of Let, to stop repetition

--- spec/tennis_scorer_spec.rb
+++ spec/tennis_scorer_spec.rb
@@ -1,20 +1,19 @@
 require "tennis_scorer"
 describe Tennis do 
+  let(:tennis) {Tennis.new}
   it "can score a new game of tennis" do 
-    tennis = Tennis.new
+    
     expect(tennis.score).to eq ('Love-Love')
   end
 
   it "can Score a game where one player has won a point" do 
 
-    tennis = Tennis.new
     tennis.won_point
     expect(tennis.score).to eq ("Fifteen-Love")
   end
 
   it "can Score a game where one player has won a point" do 
 
-    tennis = Tennis.new
     tennis.won_point
     tennis.won_point
     expect(tennis.score).to eq ("Thirty-Love")
